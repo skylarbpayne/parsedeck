e676c41ca7b7207d00e4bab9a6b5556397d77512
Skylar Payne <skylar.b.payne@gmail.com>
2024-10-09 14:10:18 -0700

    Setup eval harness for the deck generation. Closes #4

---
+++ parsedeck/evals/dataset_urls.py
@@ -0,0 +1,9 @@
+_URLS = [
+    "https://www.astronomer.io/docs/learn/dags",
+    "https://mirascope.com/docs/WHY/",
+    "https://aftercompsci.com/2024/10/05/work-steroids-ai-tools-and-models/",
+    "https://seattledataguy.substack.com/p/back-to-the-basics-with-sql-understanding",
+    "https://seattledataguy.substack.com/p/from-basics-to-challenges-a-data",
+]
+
+DATASET = [{"input": url} for url in _URLS]
--- parsedeck/evals/eval_deck_cards.py
+++ parsedeck/evals/eval_deck_cards.py
@@ -1,32 +1,31 @@
 import braintrust
 
-from parsedeck.deck import make_deck_plan
+from parsedeck.deck import make_card, make_deck_plan
 from parsedeck.evals.dataset_urls import DATASET
 from parsedeck.main import get_url_content
 
 
 def run_evaluation(dataset: list[braintrust.EvalCase]):
-    experiment = braintrust.init(project="FlashCardGen")  # Replace with your project name
+    experiment = braintrust.init(project="FlashCardGenDeckCards")  # Replace with your project name
 
     for data in dataset:
-        with experiment.start_span(name="plan_deck") as span:
+        with experiment.start_span(name="generate_deck_cards") as span:
             x = data["input"]
             content = get_url_content(x)
             deck_plan = make_deck_plan(content)
+            cards = [make_card(content, card_description) for card_description in deck_plan.cards_to_create]
 
             metadata = {
                 "url": x,
-                # "num_revisions": len(deck_plan.plan_revisions),
-                "final_num_cards": len(deck_plan.cards_to_create),
+                "plan": deck_plan,
                 "content_length": len(content),
-                "final_plan": deck_plan.plan,
-                # "final_feedback": deck_revisions.plan_revisions[-1].feedback,
-                # "num_cards": [len(revision.cards_to_create) for revision in deck_revisions.plan_revisions],
+                "num_cards": len(cards),
+                "num_planned_cards": len(deck_plan.cards_to_create),
             }
 
             span.log(
                 input=content,
-                output=deck_plan,
+                output=cards,
                 # scores={
                 #     'completeness': completeness,
                 #     # scope,
